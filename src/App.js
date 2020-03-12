f96d8960df02cfaa79bb7af57eb8e4c4ae4de09f
Allen <ajbest91@gmail.com>
2020-03-11 21:49:56 -0400

    Styling Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from "./robots";
+import './App.css';
 
 class App extends Component {
 	constructor(){
@@ -20,10 +21,10 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots =>{
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		
+
 		return (
 			<div className="tc">
-		    	<h1>RoboFriends</h1>
+		    	<h1 className="f1">RoboFriends</h1>
 		    	<SearchBox searchChange={ this.onSearchChange }/>
 		    	<CardList robots={ filteredRobots }/>
 		    </div>
